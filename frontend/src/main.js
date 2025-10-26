9f339099a55e081de56d5fff73687f2c6ac77100
Renan Teixeira <renanpugas@gmail.com>
2025-10-26 14:33:24 -0300

    add-vuetify-and-tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,14 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css';
+import './tailwind.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
